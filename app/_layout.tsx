87581e24ba04ccf61a2976b4ebcc277dbf2571e4
Pinindu Nuwandhara <158325751+Pinindu20@users.noreply.github.com>
2024-11-30 10:25:50 +0530

    Update bottom bar using navigation and icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
-import { Link, Slot } from "expo-router";
-import { View, Text } from "react-native";
+import { Link, router, Slot } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 import "../global.css";
 
 // export default Slot;
@@ -8,20 +9,29 @@ const RootLayout = () => {
   return (
     <View className="relative w-full flex-1">
         <Slot />
-        <View className="w-full px-5 absolute h-12 bottom-0
-        left-0 z-50 bg-red-500 flex-row items-center justify-between">
-          <View>
-            <Link href={"/"}>Home</Link>
-          </View>
-          <View>
-            <Link href={"/product"}>product</Link>
-          </View>
-          <View>
-            <Link href={"/category"}>category</Link>
-          </View>
-          <View>
-            <Link href={"/profile"}>profile</Link>
-          </View>
+        <View className="w-full px-5 absolute h-16 bottom-0
+        left-0 z-50 bg-gray-300 drop-shadow-md flex-row items-center justify-between">
+
+          <TouchableOpacity onPress={()=>router.navigate('/')} className="items-center">
+          <Ionicons name="home-outline" size={24} color="black" />
+            <Text>Home</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={()=>router.navigate('/product')} className="items-center">
+          <Ionicons name="logo-dropbox" size={24} color="black" />
+            <Text>product</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={()=>router.navigate('/category')} className="items-center">
+          <Ionicons name="grid-outline" size={24} color="black" />
+            <Text>category</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={()=>router.navigate('/profile')} className="items-center">
+          <Ionicons name="person-outline" size={24} color="black" />
+            <Text>profile</Text>
+          </TouchableOpacity>
+
         </View>
     </View>
   )
